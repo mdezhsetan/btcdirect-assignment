e59edac69969f682f191179192a4ac3d67d09b6e
Mahshad <mahshad.dezhsetan@gmail.com>
2025-10-17 18:51:12 +0200

    remove cookie wall assertion

--- tests/access-control.spec.ts
+++ tests/access-control.spec.ts
@@ -1,6 +1,5 @@
 import test from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
-import { LandingPage } from '../pages/landingPage';
 import { Locale } from './test-data/locale.data';
 import { Tag } from '../tags/tags';
 
@@ -8,11 +7,9 @@ test(
   'verify non logged in user redirected to login',
   { tag: [Tag.Authentication] },
   async ({ page }) => {
-    const landingPage = new LandingPage(page, Locale.En);
     const loginPage = new LoginPage(page, Locale.En);
 
     await loginPage.visit('personal-information');
-    await landingPage.acceptCookies();
     await loginPage.expectUserIsOnLoginPage();
   },
 );
